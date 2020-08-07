0a66269ca820123daa6af013d315df100fff1f86
Karim Gueye <kgueye2000@gmail.com>
2020-08-07 13:31:45 -0400

    modified:   RSO.py

--- RSO.py
+++ RSO.py
@@ -1,5 +1,25 @@
 import sys, os, requests, calendar, arrow
 from pathlib import Path
+from enum import Enum
+
+unsave = False
+user = 'KageyKay'
+password = ''
+key = ''
+secret = ''
+subs = []
+scrub = []
+count = 0
+
+class PostValue(Enum):
+    # The two posts are entirely different
+    UNIQUE = 1
+    # The two posts share a title and nothing else
+    TITLE = 2
+    # The two posts share a title, and were posted at the same time, in the same place, by the same author
+    EDITED = 3
+    # The two posts are identical
+    DUPLICATE = 4
 
 def savePost():
     while (True):
@@ -42,19 +62,18 @@ def savePost():
                 try:
                     # Make this less stupid pls
                     # Truncate the title only if the filename is near windows character limit
-                    filePath = Path(subreddit) / f"{title[:25]}"
+                    
                     fileNum = 1
+                    filePath = namePost(title, subreddit)
                     
                     # This block of code should only run if the files are proven to be a different post, otherwise the post should be ignored/overwritten
                     # If the files have the same name, author, and time, and the filesize is different create a new file called "{title} (edit)"
                     # If the contents are the same leave the old file unedited and skip over the post
+                    
+                    
                     if ((subreddit / Path(filePath.name + ".txt")).is_file()):
                         oldPost = open(os.path.abspath(filePath) + ".txt", encoding='utf-8')
-                        """
-                        print(oldPost.read())
-                        print('\n----------------\n')
-                        print(content)
-                        """
+                    
                         
                         hSize = len(header.encode('utf-8'))
                         cSize = len(content.encode('utf-8'))
@@ -97,17 +116,28 @@ def nextPage():
         response = requests.get(f'https://oauth.reddit.com/user/{user}/saved?raw_json=1&limit=100&count={count}&after={after}', headers=headers)
         return 1
     
+#returns ABSOLUTE Filepath of saved post
 def namePost(title, subreddit):
+    name = Path.cwd() / Path(subreddit) / title
+    name = os.path.dirname(name)
+    if(len(name) < 250):
+        return Path(name)
+    else:
+        name = name[:250]
+        return Path(name)
+    
+# -1 = Different File
+# 1 = Same File
+# 2 = Same Post, different contents
 
-unsave = False
-user = 'KageyKay'
-password = ''
-key = ''
-secret = ''
-subs = []
-scrub = []
-count = 0
 
+def checkContent(filePath):
+    if ((filePath / ".txt").is_file()):
+        oldPost = open(filePath / ".txt", encoding='utf-8')
+        oldPost.close()
+    else:
+        return PostValue.UNIQUE
+        
 if (len(sys.argv) > 1):
     for arg in sys.argv[1:]:
         if arg == "-unsave":
@@ -123,6 +153,8 @@ data = {'grant_type': 'password', 'username': user, 'password': password}
 auth = requests.auth.HTTPBasicAuth(key, secret)
 r = requests.post('https://www.reddit.com/api/v1/access_token', data=data, headers={'user-agent':'RSO.py by kr0bat'}, auth=auth)
 d = r.json()
+
+print(checkContent(Path('test')))
 
 if 'error' in d.keys():
     print(d['error'])
